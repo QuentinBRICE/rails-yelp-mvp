0278937ff2505e4a9a025f204a876a2b932c78f5
Quentin Brice <quentin.brice.qb19@gmail.com>
2023-11-17 11:48:25 +0100

    fix push

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: 'japanese' }
+pizza_east = { name: "Pizza East", address: '56A Shoreditch High St, London E1 6PQ', category: 'italian' }
+moulox = { name: 'Moulox', address: "10 avenue des Champs-Elysées", category: 'french' }
+roi_dla_frite = { name: 'Roi Dla Frite', address: '30 Shoreditch High St, Quimper', category: 'belgian' }
+chistreet = { name: 'ChiStreet', address: '40 Boundary St, London E2 7JE', category: 'chinese' }
+
+[dishoom, pizza_east, moulox, roi_dla_frite, chistreet].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
